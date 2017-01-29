db34f24697d7a4b13e89ef1a9999f4152a27765a
Jimmie Harkins <ghana796@gmail.com>
2017-01-29 14:33:06 -0500

    Added descriptive variable names in main stuff

--- ConsoleScoutDecoder/src/Main.java
+++ ConsoleScoutDecoder/src/Main.java
@@ -12,56 +12,61 @@ public class Main {
 		while(true) {
 			String input = sc.nextLine();
 			String[] inputArray = input.split(" ");
-			switch(inputArray[0]) {
-			case "addTeam":
-				if(Match.isNumeric(inputArray[1])) {
-					if(teamExists(Integer.parseInt(inputArray[1]), teams) == -1) {
-						teams.add(new Team(Integer.parseInt(inputArray[1])));
-						System.out.println("Added team " + inputArray[1]);
+			switch(inputArray[0]) { //inputArray[0] is the command itself
+			case "addTeam": 
+				String teamNumber = inputArray[1];
+				if(Match.isNumeric(teamNumber)) {
+					if(teamExists(Integer.parseInt(teamNumber), teams) == -1) {
+						teams.add(new Team(Integer.parseInt(teamNumber)));
+						System.out.println("Added team " + teamNumber);
 					} else {
-						System.out.println("Team " + inputArray[1] + " already exists!");
+						System.out.println("Team " + teamNumber + " already exists!");
 					}
 				} else {
-					System.out.println(inputArray[1] + " is not a number. That argument must be a number.");
+					System.out.println(teamNumber + " is not a number. That argument must be a number.");
 				}
 				break;
 			case "addMatch":
-				if(Match.isNumeric(inputArray[1])) {
-					if(teamExists(Integer.parseInt(inputArray[1]), teams) != -1) {
-						if(isLegalMatch(inputArray[2])) {
-							teams.get(teamExists(Integer.parseInt(inputArray[1]), teams)).addMatch(inputArray[2]);
-							System.out.println("Added match " + inputArray[2]);
+				String teamToAdd = inputArray[1];
+				String matchString = inputArray[2];
+				if(Match.isNumeric(teamToAdd)) {
+					if(teamExists(Integer.parseInt(teamToAdd), teams) != -1) {
+						if(isLegalMatch(matchString)) {
+							teams.get(teamExists(Integer.parseInt(teamToAdd), teams)).addMatch(matchString);
+							System.out.println("Added match " + matchString);
 						} else {
 							System.out.println("This is not a legal match format.");
 						}
 					} else {
-						System.out.println("Team " + inputArray[1] + " does not exist!");
+						System.out.println("Team " + teamToAdd + " does not exist!");
 					}
 				} else {
-					System.out.println(inputArray[1] + " is not a number. That argument must be a number.");
+					System.out.println(teamToAdd + " is not a number. That argument must be a number.");
 				}
 				break;
 			case "printTeam":
-				if(Match.isNumeric(inputArray[1])) {
-					int teamNumber = Integer.parseInt(inputArray[1]);
-					if(teamExists(teamNumber, teams) != -1) {
-						System.out.println(teams.get(teamExists(Integer.parseInt(inputArray[1]),teams)));
+				String teamNumberToPrint = inputArray[1];
+				if(Match.isNumeric(teamNumberToPrint)) {
+					int teamNumberAsInt = Integer.parseInt(teamNumberToPrint);
+					if(teamExists(teamNumberAsInt, teams) != -1) {
+						System.out.println(teams.get(teamExists(Integer.parseInt(teamNumberToPrint),teams)));
 					} else {
-						System.out.println("Team " + teamNumber + " does not exist.");
+						System.out.println("Team " + teamNumberAsInt + " does not exist.");
 					}
 					
 				} else {
-					System.out.println(inputArray[1] + " is not a number. That argument must be a number.");
+					System.out.println(teamNumberToPrint + " is not a number. That argument must be a number.");
 				}
 				break;
-			case "rank":
-				if(Match.isKey(inputArray[1])) {
-					sortTeams(inputArray[1], teams);
+			case "sort":
+				String keyToSort = inputArray[1];
+				if(Match.isKey(keyToSort)) {
+					sortTeams(keyToSort, teams);
 					for(Team t : teams) {
 						System.out.println(t);
 					}
 				} else {
-					System.out.println("" + inputArray[1] + " is not a sortable key.");
+					System.out.println("" + keyToSort + " is not a sortable key.");
 				}
 				break;
 			default:
@@ -85,15 +90,6 @@ public class Main {
 			Collections.sort(teams, (a,b) -> (Integer)a.getAverage(key) < (Integer)b.getAverage(key) ? -1 : (Integer)a.getAverage(key) == (Integer)b.getAverage(key) ? 0 : 1);
 		} else if(teams.get(0).getDataArray(key).get(0) instanceof Boolean) {
 			//eventually make to sort by most true
-//			ArrayList<Team> temp = new ArrayList<Team>();
-//			for(Team t : teams) {
-//				if((Boolean)t.getAverage(key)) {
-//					temp.add(0,t);
-//				} else {
-//					temp.add(t);
-//				}
-//			}
-//			teams = temp;
 			for(int i = 0; i < teams.size();i++) {
 				if (!(Boolean)teams.get(i).getAverage(key)) {
 					for(int j = i + 1; j<teams.size();j++) {
